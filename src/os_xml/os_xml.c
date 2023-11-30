19354218a93f71702035aae115afcb8d26c37a09
LucioDonda <donda.lucio@gmail.com>
2023-11-30 16:10:18 +0000

    Simplifying white space check

--- src/os_xml/os_xml.c
+++ src/os_xml/os_xml.c
@@ -288,7 +288,7 @@ static int _ReadElem(unsigned int parent, OS_XML *_lxml, unsigned int recursion_
 
     // consume all the spaces, tabs or new line characters
     while ((c = xml_getc_fun(_lxml->fp, _lxml)) != cmp) {
-        if(isspace(c) || c == '\n' || c == '\r') {
+        if(isspace(c)) {
             continue;
         } else {
             break;
